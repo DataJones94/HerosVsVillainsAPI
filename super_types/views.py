64b0aa588df20317a0e6105b35acbd104048ebad
unknown <k.jones45@yahoo.com>
2023-02-20 12:21:21 -0500

    Created views.py and urls.py for both supers and super_types apps. + worked on methods on views.py file for super_types.

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SuperType
+from .serializers import SuperTypeSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def super_types_list(request):
+    if request.method == 'GET':
+        # dealership_name = request.query_params.get('dealership')
+        # print(dealership_name)
+
+        queryset = SuperType.objects.all()
+
+        # if dealership_name:
+        #     queryset = queryset.filter(dealership__name= dealership_name)
+
+        serializer = SuperTypeSerializer(queryset, many=True)    
+        return Response(serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
